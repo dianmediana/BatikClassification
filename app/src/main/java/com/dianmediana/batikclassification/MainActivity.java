dc85bbfc27264dfa0eb981fbd700fd89312809f3
dianmediana <82147780+dianmediana@users.noreply.github.com>
2022-12-07 12:24:54 +0800

    deploy model efficientnet

--- app/src/main/java/com/dianmediana/batikclassification/MainActivity.java
+++ app/src/main/java/com/dianmediana/batikclassification/MainActivity.java
@@ -1,10 +1,5 @@
 package com.dianmediana.batikclassification;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -14,13 +9,19 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import com.dianmediana.batikclassification.ml.Model;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -30,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
     TextView result;
-    int imageSize = 32;
+    int imageSize = 224;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,51 +67,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void classifyImage(Bitmap image){
-//        try {
-//            Model model = Model.newInstance(getApplicationContext());
-//
-//            // Creates inputs for reference.
-//            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 32, 32, 3}, DataType.FLOAT32);
-//            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
-//            byteBuffer.order(ByteOrder.nativeOrder());
-//
-//            int[] intValues = new int[imageSize * imageSize];
-//            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
-//            int pixel = 0;
-//            //iterate over each pixel and extract R, G, and B values. Add those values individually to the byte buffer.
-//            for(int i = 0; i < imageSize; i ++){
-//                for(int j = 0; j < imageSize; j++){
-//                    int val = intValues[pixel++]; // RGB
-//                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 1));
-//                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 1));
-//                    byteBuffer.putFloat((val & 0xFF) * (1.f / 1));
-//                }
-//            }
-//
+        try {
+            Model model = Model.newInstance(getApplicationContext());
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            int[] intValues = new int[imageSize * imageSize];
+            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+            int pixel = 0;
+            //iterate over each pixel and extract R, G, and B values. Add those values individually to the byte buffer.
+            for(int i = 0; i < imageSize; i ++){
+                for(int j = 0; j < imageSize; j++){
+                    int val = intValues[pixel++]; // RGB
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 1));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 1));
+                    byteBuffer.putFloat((val & 0xFF) * (1.f / 1));
+                }
+            }
+
 //            inputFeature0.loadBuffer(byteBuffer);
-//
-//            // Runs model inference and gets result.
-//            Model.Outputs outputs = model.process(inputFeature0);
-//            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
-//
-//            float[] confidences = outputFeature0.getFloatArray();
-//            // find the index of the class with the biggest confidence.
-//            int maxPos = 0;
-//            float maxConfidence = 0;
-//            for (int i = 0; i < confidences.length; i++) {
-//                if (confidences[i] > maxConfidence) {
-//                    maxConfidence = confidences[i];
-//                    maxPos = i;
-//                }
-//            }
-//            String[] classes = {"Apple", "Banana", "Orange"};
-//            result.setText(classes[maxPos]);
-//
-//            // Releases model resources if no longer used.
-//            model.close();
-//        } catch (IOException e) {
-//            // TODO Handle the exception
-//        }
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            Model.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = ((Model.Outputs) outputs).getOutputFeature0AsTensorBuffer();
+
+            float[] confidences = outputFeature0.getFloatArray();
+            // find the index of the class with the biggest confidence.
+            int maxPos = 0;
+            float maxConfidence = 0;
+            for (int i = 0; i < confidences.length; i++) {
+                if (confidences[i] > maxConfidence) {
+                    maxConfidence = confidences[i];
+                    maxPos = i;
+                }
+            }String[] classes = {"Batik Tambal", "Batik Sekar Jagad", "Batik Poleng", "Batik Parang", "Batik Pala","Batik Megamendung",
+                    "Batik Lasem", "Batik Kawung", "Batik Insang", "Batik Ikat Celup", "Batik Geblek Renteng", "Batik Dayak",
+                    "Batik Cendrawasih", "Batik Betawi", "Batik Bali"};
+            result.setText(classes[maxPos]);
+
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
     }
 
     @Override
@@ -134,6 +137,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 imageView.setImageBitmap(image);
 
+//                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
                 image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
                 classifyImage(image);
             }
